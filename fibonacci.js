26fdf1a53f80da84ebc86377d48cd347d4207ac6
Daniel Weber <weberdan@gmail.com>
2014-10-04 17:36:37 -0400

    cleanup, add a test case with no cache

--- fibonacci.js
+++ fibonacci.js
@@ -1,12 +1,22 @@
 fibonacci = {
+    // This fibonacci implementation, just to be different, only
+    // caches two numbers every 20 inputs.  This is still fast enough
+    // that, when counting up, each number can be calculated in less
+    // than two milliseconds (in nodejs on my relatively underpowered
+    // VM).
+
+    // NB: JavaScript's integer class will start dropping digits around
+    // fib(79).
     cache: {0: 0, 1: 1},
+    clear_cache: function() {
+	this.cache = {0: 0, 1: 1};
+    },
     calculate: function(n) {
 	if (n in this.cache)
 	    return this.cache[n];
-	ret = this.calculate(n-1) + this.calculate(n-2);
-	if (n % 20 == 0 || n % 20 == 1) {
+	ret = this.calculate(n-2) + this.calculate(n-1);
+	if (n % 20 == 0 || n % 20 == 1)
 	    this.cache[n] = ret;
-	}
 	return ret;
     }
 };
--- test-fib.js
+++ test-fib.js
@@ -5,8 +5,18 @@ f = fibonacci;
 for (var i = 0; i < 100; i++) {
     // around fib(79) exceed JavaScript's Integer range so precision will be lost.
     var start = new Date().getTime();
-    ans = f.calculate(i);
+    var ans = f.calculate(i);
     var end = new Date().getTime();
     var millisec = end - start;
     console.log(i + "\t" + millisec + "\t" + ans);
 }
+console.log(f.cache);
+
+// try again, with a cleared cache
+f.clear_cache();
+var start = new Date().getTime();
+var ans = f.calculate(65);
+var end = new Date().getTime();
+var millisec = end - start;
+
+console.log("65->\t" + millisec + "\t" + ans);
