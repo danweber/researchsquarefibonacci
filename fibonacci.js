ae8b135d030044ec2872f61706010a66d3d2fefb
Daniel Weber <weberdan@gmail.com>
2014-10-04 17:23:22 -0400

    notes about JS int, and less frequent caching

--- fibonacci.js
+++ fibonacci.js
@@ -1,11 +1,12 @@
 fibonacci = {
     cache: {0: 0, 1: 1},
     calculate: function(n) {
-//	console.log(n);
 	if (n in this.cache)
 	    return this.cache[n];
 	ret = this.calculate(n-1) + this.calculate(n-2);
-	this.cache[n] = ret;
+	if (n % 20 == 0 || n % 20 == 1) {
+	    this.cache[n] = ret;
+	}
 	return ret;
     }
 };
--- test-fib.js
+++ test-fib.js
@@ -1,6 +1,9 @@
+// This runs in nodejs.
 require('./fibonacci.js')
+
 f = fibonacci;
 for (var i = 0; i < 100; i++) {
+    // around fib(79) exceed JavaScript's Integer range so precision will be lost.
     var start = new Date().getTime();
     ans = f.calculate(i);
     var end = new Date().getTime();
